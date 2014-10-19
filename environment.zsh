bc9a9e2bfb6ca6b71fdbf6b38807f2a2003dc55f
Leonardo Melo <leomeloxp@gmail.com>
2014-10-19 08:23:56 +0100

    Disabled TMUX auto start

--- environment.zsh
+++ environment.zsh
@@ -49,12 +49,12 @@ PATH="${PATH}:/srv/git/imgur"
 PATH="/usr/local/heroku/bin:$PATH"
 
 #TMUX
-if which tmux >/dev/null 2>&1; then
+#if which tmux >/dev/null 2>&1; then
     # if no session is started, start a new session
-    test -z ${TMUX} && tmux
+#    test -z ${TMUX} && tmux
 
     # when quitting tmux, try to attach
 #    while test -z ${TMUX}; do
 #        tmux attach || break
 #    done
-fi
+#fi
